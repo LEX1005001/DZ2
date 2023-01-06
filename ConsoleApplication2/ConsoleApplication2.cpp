04439231d881cf745596fb1bec7e2f084cf63ee3
LEX1005001 <AleX69.galkin@yandex.ru>
2023-01-06 18:13:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int km = 0;
+	int hour = 0;
+
+	cout << "distance in km: ";
+	cin >> km;
+	cout << "time in hours: ";
+	cin >> hour;
+	cout << "You need to go at speed--> " << km / hour << " km/h";
+}
